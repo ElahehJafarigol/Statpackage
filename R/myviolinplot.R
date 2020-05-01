1896e71c36efe0d5b06d08b1adfb946d63b03b84
Elaheh Jafarigol <elaheh.jafarigol@ou.edu>
2020-04-30 20:29:28 -0500

    plot function

--- R/myviolinplot.R
+++ R/myviolinplot.R
@@ -1,3 +1,5 @@
+library(vioplot)
+library(zoo)
 #' A function to create a violin plot to compare the two samples
 #'
 #' @param x : vector of sample data from population 1
@@ -9,8 +11,6 @@
 #' @examples
 #' x = rnorm(30,10,12); y = rnorm(40,20,15); myviolinplot(x,y)
 #'
-library(vioplot)
-library(zoo)
 myviolinplot <- function(x, y){
   title <- ("Violin plot of sample x and y")
   vioplot(x, y, names=c("Sample x", "Sample y"), main = title, col="gold")
